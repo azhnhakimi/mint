aef8072cad4808ed60c5b1f1dff7349f4b53ac48
azhnhakimi <azhanhakimii@gmail.com>
2025-11-21 11:13:22 +0800

    testing with flash messages

--- app/(tabs)/budget/index.tsx
+++ app/(tabs)/budget/index.tsx
@@ -1,6 +1,8 @@
 import { SignOutButton } from "@/components/SignOutButton";
 import { useAuth, useUser } from "@clerk/clerk-expo";
-import { Text } from "react-native";
+import Constants from "expo-constants";
+import { Text, TouchableOpacity } from "react-native";
+import { showMessage } from "react-native-flash-message";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const BudgetIndex = () => {
@@ -18,6 +20,25 @@ const BudgetIndex = () => {
         Feature coming soon!
       </Text>
       <SignOutButton />
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#212122",
+          padding: 10,
+          marginTop: 20,
+          borderRadius: 5,
+        }}
+        onPress={() => {
+          showMessage({
+            message: "This is a flash message",
+            description: "Simple message",
+            type: "danger",
+            statusBarHeight: Constants.statusBarHeight,
+          });
+        }}
+      >
+        <Text style={{ color: "#fff" }}>Press Me</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Redirect, Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import FlashMessage from "react-native-flash-message";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 
@@ -20,6 +21,7 @@ export default function RootLayout() {
 
         <Slot />
       </SafeAreaProvider>
+      <FlashMessage position="top" />
     </ClerkProvider>
   );
 }
